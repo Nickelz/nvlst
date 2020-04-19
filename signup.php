993e8e363f672198c38c77f9c04f29206e2731b0
MK <46850924+97MK@users.noreply.github.com>
2020-04-19 19:25:48 +0300

    create default wishlist

--- signup.php
+++ signup.php
@@ -28,10 +28,14 @@ if(isset($_POST["Username"])) {
 		\"{$hashed_password}\"
 	);";
 
+    
 	echo $query . "<br>";
 
 	if ($conn -> query($query) === TRUE) {
         $user -> login($arr['Email'], $arr['Password']);
+
+    
+        mysqli_query($conn,"INSERT INTO `wishlist` (`UserID`) VALUES ('$_SESSION[UserID]');");
 		header('Location: discover.php?action=registered');
 		exit;
 	} else {
